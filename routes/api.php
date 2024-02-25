181a2e7ad11f21038fbbe320db1a9599a3d84fa2
Jesús Hernández <jehersi988@gmail.com>
2024-02-25 10:55:31 -0500

    feat: route services provider

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('users', \Cms\User\Controller\UserController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
